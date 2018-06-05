bc8dbd3ade0c8c84865a5e2df5f49a4897d34782
YassinRian <yassinrian@hotmail.com>
2018-06-05 19:46:05 +0200

    filter cascading
    cascading logica toevoegen

--- src/dynamicFilter.js
+++ src/dynamicFilter.js
@@ -1,8 +1,17 @@
 $j(document).ready(function () {
 
+// ******    variables  ******
+  var selectieLijstKPL1 = $j(".prmt_kpl1").find('select option');
   var selectieLijstKPL2  = $j(".prmt_kpl2").find('select option');
+  var selectieLijstKPL3 = $j(".prmt_kpl3").find('select option');
 
-/*
+  var selectieLijstKDR1 = $j(".prmt_kdr1").find('select option');
+  var selectieLijstKDR2 = $j(".prmt_kdr2").find('select option');
+  var selectieLijstKDR3 = $j(".prmt_kdr3").find('select option');
+
+
+
+// *****  functies  ******
   var delay = (function () {
     var timer = 0;
     return function (callback, ms) {
@@ -10,13 +19,12 @@ $j(document).ready(function () {
       timer = setTimeout(callback, ms);
     };
   })();
-*/
 
-  function FilterSelectList(v_input, v_selectieList) {
 
+  function FilterSelectList(v_input) {
 
     var var_input = $j.trim($j("#" + v_input).val().toLowerCase()); // input wordt ingelezen, spaties worden weggehaald en alles wordt naar kleine letters gezet.
-    var var_selectieList = $j("." + v_selectieList).find('select option');
+    var var_selectieList = selectieLijstKPL2
 
     if (var_input.length === 0) { // als input leeg is dan zijn alle regels van niv1 zichtbaar
       var_selectieList.show();
@@ -30,16 +38,70 @@ $j(document).ready(function () {
   } // function closure
 
 
+function cascadingLogic () {
+
+  // map functie doet een loop over alle waarden die zichtbaar zijn en pakt de val op
+  var x = selectie_Niv1.map(function () {
+    if ($j(this).css('display') !== 'none')
+      return $j(this).val() //extract de "value" onderdeel
+  }).get();
+
+  var arr_x = []; // declaratie van een lege array
+  arr_x.push(x) // toevoegen van de val waarden in de array
+  //console.log(arr_x[0]); // test 
+
+  // hier komt de filtering van Alle waarden => elke waarde in de array wordt gecheckt met de text uit options_alle 
+  var z = options_alle.hide().filter(function () {
+    if (arr_x[0].some(x => $j(this).text().indexOf(x) > -1)) { // some geeft een true of fals als er wordt voldaan aan de functie => pasop nieuwe syntax werkt niet in IE11 denk ik
+      return $j(this).text();
+    }
+  }).show();
+  //console.log(z); test
+
+  // map functie doet een loop over alle waarden uit de PRMT_alle die zichtbaar zijn en pakt de text waarde op en geeft het door aan variable Y
+  var y = options_alle.map(function () {
+    if ($j(this).css('display') !== 'none')
+      return $j(this).val()
+  }).get();
+
+  var arr_y = []; // declaratie van een lege array
+  arr_y.push(y) // toevoegen van de val waarden in de array
+  //console.log(arr_y[0]); // test
+
+  //map functie loopt over arr_y en split de juiste waarde om deze later door te geven aan Niv2 
+  var T = arr_y[0].map(function (val) { // val is de waarde van de option uit arr_y
+    var split_Waarde = val.split('|');
+    return split_Waarde[1]
+  });
+
+  var arr_A = [];
+  arr_A.push(T);
+  console.log(arr_A[0]);
+
+  // hier komt de filtering van Niv2 waarden => elke waarde (x) in arr_A wordt gecheckt met de text uit Niv2 ( $(this).val() ) option waarde
+  var z = options_niv2.hide().filter(function () {
+    if (arr_A[0].some(x => $j(this).val().indexOf(x) > -1)) { // some geeft een true of fals als er wordt voldaan aan de functie => pasop nieuwe syntax werkt niet in IE11 denk ik
+      return $j(this).text();
+    }
+  }).show();
+    //console.log(z); test
+
+
+}
+ 
+
+
+
+
+
+
   $j('#input_kpl2').bind('change keyup', function () {
-
-    console.log("hallo Yassin");
-
-    FilterSelectList('input_kpl2', 'prmt_kpl2')
-
-/*    delay(function () {
-      FilterSelectList('input_kpl2', 'prmt_kpl2')
+    
+    delay(function () {
+       FilterSelectList('input_kpl2')
     }, 500); 
-*/
+    cascadingLogic('selectie_niv1', 'selectie_niv2', 'selectie');
+  
   });
 
 });
