4ef73ec34c55813920645631b55df41912975413
YassinRian <yassinrian@hotmail.com>
2018-06-06 11:41:33 +0200

    filter cascading 2
    wijzigingen

--- src/dynamicFilter.js
+++ src/dynamicFilter.js
@@ -1,17 +1,27 @@
 $j(document).ready(function () {
 
-// ******    variables  ******
+  // ******    variables  ******
   var selectieLijstKPL1 = $j(".prmt_kpl1").find('select option');
-  var selectieLijstKPL2  = $j(".prmt_kpl2").find('select option');
+  var selectieLijstKPL2 = $j(".prmt_kpl2").find('select option');
   var selectieLijstKPL3 = $j(".prmt_kpl3").find('select option');
 
   var selectieLijstKDR1 = $j(".prmt_kdr1").find('select option');
   var selectieLijstKDR2 = $j(".prmt_kdr2").find('select option');
   var selectieLijstKDR3 = $j(".prmt_kdr3").find('select option');
+  var selectie_Allewaarden = $j(".allewaarden").find('select option');
+
+//              **** INIT ****
+$j('#input_kpl2').bind('change keyup', function () {
+
+    delay(function () {
+      FilterSelectList('input_kpl2', selectieLijstKPL2)
+    }, 500);
+    cascadingLogic(selectieLijstKPL2, selectieLijstKPL3, 'selectie');
+
+  });
 
 
-
-// *****  functies  ******
+//             *****  functies  ******
   var delay = (function () {
     var timer = 0;
     return function (callback, ms) {
@@ -21,10 +31,10 @@ $j(document).ready(function () {
   })();
 
 
-  function FilterSelectList(v_input) {
+  function FilterSelectList(v_input, v_selectielist) {
 
     var var_input = $j.trim($j("#" + v_input).val().toLowerCase()); // input wordt ingelezen, spaties worden weggehaald en alles wordt naar kleine letters gezet.
-    var var_selectieList = selectieLijstKPL2
+    var var_selectieList = v_selectielist;
 
     if (var_input.length === 0) { // als input leeg is dan zijn alle regels van niv1 zichtbaar
       var_selectieList.show();
@@ -38,10 +48,18 @@ $j(document).ready(function () {
   } // function closure
 
 
-function cascadingLogic () {
+function cascadingLogic (v_niv_alle, v_niv1, v_niv2, v_niv3) {
 
+var niv1 = v_niv1;
+var niv2 = v_niv2;
+var niv3 = v_niv3;
+var niv_alle = v_niv_alle;
+
+var arr_x = [];
+var arr_y = [];
+var 
   // map functie doet een loop over alle waarden die zichtbaar zijn en pakt de val op
-  var x = selectie_Niv1.map(function () {
+  var x = niv1.map(function () {
     if ($j(this).css('display') !== 'none')
       return $j(this).val() //extract de "value" onderdeel
   }).get();
@@ -51,7 +69,7 @@ function cascadingLogic () {
   //console.log(arr_x[0]); // test 
 
   // hier komt de filtering van Alle waarden => elke waarde in de array wordt gecheckt met de text uit options_alle 
-  var z = options_alle.hide().filter(function () {
+  var z = niv_alle.hide().filter(function () {
     if (arr_x[0].some(x => $j(this).text().indexOf(x) > -1)) { // some geeft een true of fals als er wordt voldaan aan de functie => pasop nieuwe syntax werkt niet in IE11 denk ik
       return $j(this).text();
     }
@@ -59,7 +77,7 @@ function cascadingLogic () {
   //console.log(z); test
 
   // map functie doet een loop over alle waarden uit de PRMT_alle die zichtbaar zijn en pakt de text waarde op en geeft het door aan variable Y
-  var y = options_alle.map(function () {
+  var y = niv_alle.map(function () {
     if ($j(this).css('display') !== 'none')
       return $j(this).val()
   }).get();
@@ -76,32 +94,18 @@ function cascadingLogic () {
 
   var arr_A = [];
   arr_A.push(T);
-  console.log(arr_A[0]);
 
   // hier komt de filtering van Niv2 waarden => elke waarde (x) in arr_A wordt gecheckt met de text uit Niv2 ( $(this).val() ) option waarde
-  var z = options_niv2.hide().filter(function () {
+  var z = niv2.hide().filter(function () {
     if (arr_A[0].some(x => $j(this).val().indexOf(x) > -1)) { // some geeft een true of fals als er wordt voldaan aan de functie => pasop nieuwe syntax werkt niet in IE11 denk ik
       return $j(this).text();
     }
   }).show();
-    //console.log(z); test
-
-
-}
- 
 
 
 
-
-
-
-  $j('#input_kpl2').bind('change keyup', function () {
-    
-    delay(function () {
-       FilterSelectList('input_kpl2')
-    }, 500); 
-    cascadingLogic('selectie_niv1', 'selectie_niv2', 'selectie');
   
-  });
+}
 
 });
+
